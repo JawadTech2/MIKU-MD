15c86a725dda89ffff85abd1705898572a9b2935
JawadTech2 <jawadbahix@outlook.com>
2024-11-22 13:09:40 +0500

    Update index.js

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ const prefix = '.'
 const ownerNumber = ['263780934873']
 
 //===================SESSION-AUTH============================
+
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
 if(!config.SESSION_ID) return console.log('Please add your session to SESSION_ID env !!')
 const sessdata = config.SESSION_ID
@@ -29,12 +30,12 @@ const filer = File.fromURL(`https://mega.nz/file/${sessdata}`)
 filer.download((err, data) => {
 if(err) throw err
 fs.writeFile(__dirname + '/auth_info_baileys/creds.json', data, () => {
-console.log("Session downloaded ✅")
+console.log("SESSION DOWNLOADED COMPLETED ✅")
 })})}
 
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 9090;
 
 //=============================================
 
